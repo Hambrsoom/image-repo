15438db7885ddd8d3f3df7a4763ce78430a5959e
Hambrsoom <hampico1234@gmail.com>
2021-01-13 01:25:54 -0500

    Cleaned the auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../entities/user.entity";
 import { AuthService } from "../services/auth.service";
-import { validate, ValidationError } from "class-validator";
 import { UserService } from "../services/user.service";
 import * as bcrypt from "bcrypt";
 
@@ -9,6 +8,7 @@ export class AuthController {
     static login = async (request: Request, response: Response) => {
       // check if username and password are set
       let { username, password } = request.body;
+
       if (!(username && password)) {
         response.sendStatus(400);
       }
@@ -17,33 +17,23 @@ export class AuthController {
       try {
         user = await UserService.getUserByUsername(username);
       } catch (error) {
-        response.sendStatus(401);
+        response.status(401).send("Either your username or password is incorrect");
       }
 
-      // check if encrypted password match
       if (!user.validatePassword(password)) {
-        response.status(401).send();
+        response.status(401).send("Either your username or password is incorrect");
         return;
       }
 
       const token:any = await AuthService.login(user);
 
-      response.status(200).json(token);
+      response.status(200).json({token:token});
     }
 
     static register = async (request: Request, response: Response) => {
-      // get parameters from the body
-      let { username, password } = request.body;
       let user:User = new User();
-      user.username = username;
-      user.password = password;
-
-      // validade if the parameters are ok
-      const errors: ValidationError[] = await validate(user);
-      if (errors.length > 0) {
-        response.status(400).send(errors);
-        return;
-      }
+      user.username = request.body["username"];
+      user.password = request.body["password"];
 
       // will generate a unique salt for each user
       const salt:any = await bcrypt.genSalt();
@@ -57,7 +47,7 @@ export class AuthController {
         await UserService.saveUser(user);
         response.status(201).send("User created");
       } catch (error) {
-        response.status(409).send("username already in use");
+        response.status(409).send("Username already in use");
       }
     }
 }
